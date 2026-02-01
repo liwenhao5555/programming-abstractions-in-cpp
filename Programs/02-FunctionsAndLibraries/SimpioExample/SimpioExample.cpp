97d141f0b9419b26a574eaa9d66351fa1c246805
liwenhao5555 <liwenhao5555@hotmail.com>
2026-02-01 19:22:31 +0800

    Update SimpioExample.cpp to document known bug in getInteger() on Linux systems and suggest alternative input method.

--- Programs/02-FunctionsAndLibraries/SimpioExample/SimpioExample.cpp
+++ Programs/02-FunctionsAndLibraries/SimpioExample/SimpioExample.cpp
@@ -3,6 +3,11 @@
  * -----------------------
  * This program is used to illustrate error-recovery in the simpio.h
  * library.
+ *
+ * NOTE (Linux): getInteger() has a known bug on Linux systems where
+ * it incorrectly rejects all input. The validation logic fails due to
+ * EOF checking issues. This is a Stanford library bug, not a C++ issue.
+ * Use standard cin >> n for input instead, or skip this example.
  */
 
 #include <iostream>
@@ -10,7 +15,7 @@
 using namespace std;
 
 int main() {
-   int n = getInteger("Enter an integer: ");
+   int n = getInteger("Enter an integer: ");  // BUG: Rejects all input on Linux
    cout << "You entered the integer " << n << endl;
    return 0;
 }
