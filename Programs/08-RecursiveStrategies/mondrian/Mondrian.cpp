d5c2d23fe98ef4b6231610d28009586cc05ccb17
liwenhao5555 <liwenhao5555@hotmail.com>
2026-04-24 23:11:27 +0800

    Enhance Mondrian.cpp to create a Mondrian-style drawing and save it as an SVG file. Introduce a local GWindow class to handle SVG output, including methods for drawing lines and managing canvas dimensions.

--- Programs/08-RecursiveStrategies/mondrian/Mondrian.cpp
+++ Programs/08-RecursiveStrategies/mondrian/Mondrian.cpp
@@ -1,11 +1,11 @@
 /*
  * File: Mondrian.cpp
  * ------------------
- * This program creates a line drawing in a style reminiscent of Mondrian.
+ * This program creates a Mondrian-style drawing and saves it as an SVG file.
  */
 
+#include <fstream>
 #include <iostream>
-#include "gwindow.h"
 #include "random.h"
 using namespace std;
 
@@ -13,6 +13,44 @@ using namespace std;
 
 const double MIN_AREA = 10000;   /* Smallest square that will be split */
 const double MIN_EDGE = 20;      /* Smallest edge length allowed       */
+
+/* Local stand-in for GWindow that writes an SVG file. */
+
+class GWindow {
+public:
+   GWindow() : width(500), height(300), out("Mondrian.svg") {
+      out << "<svg xmlns=\"http://www.w3.org/2000/svg\" "
+          << "width=\"" << width << "\" "
+          << "height=\"" << height << "\" "
+          << "viewBox=\"0 0 " << width << " " << height << "\">\n";
+      out << "<rect width=\"100%\" height=\"100%\" fill=\"white\" />\n";
+   }
+
+   ~GWindow() {
+      out << "</svg>\n";
+   }
+
+   double getWidth() const {
+      return width;
+   }
+
+   double getHeight() const {
+      return height;
+   }
+
+   void drawLine(double x0, double y0, double x1, double y1) {
+      out << "<line x1=\"" << x0
+          << "\" y1=\"" << y0
+          << "\" x2=\"" << x1
+          << "\" y2=\"" << y1
+          << "\" stroke=\"black\" stroke-width=\"2\" />\n";
+   }
+
+private:
+   double width;
+   double height;
+   ofstream out;
+};
 
 /* Function prototypes */
 
