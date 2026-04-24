71e338b485e34add7e48dbbf9ca8227438a2de2d
liwenhao5555 <liwenhao5555@hotmail.com>
2026-04-24 23:16:51 +0800

    Enhance Snowflake.cpp to draw a Koch fractal snowflake and save it as an SVG file. Introduce local GPoint and GWindow classes for SVG output management, including methods for drawing lines and handling canvas dimensions.

--- Programs/08-RecursiveStrategies/Snowflake/Snowflake.cpp
+++ Programs/08-RecursiveStrategies/Snowflake/Snowflake.cpp
@@ -1,18 +1,79 @@
 /*
  * File: Snowflake.cpp
  * -------------------
- * This program draws a Koch fractal snowflake.
+ * This program draws a Koch fractal snowflake and saves it as an SVG file.
  */
 
+#include <fstream>
 #include <iostream>
 #include <cmath>
-#include "gwindow.h"
 using namespace std;
 
 /* Constants */
 
 const double SIZE = 200;         /* Size of the order 0 fractal in pixels */
 const int ORDER = 4;             /* Order of the fractal snowflake        */
+
+/* Local stand-ins for GPoint/GWindow that write an SVG file. */
+
+class GPoint {
+public:
+   GPoint(double x = 0, double y = 0) : x(x), y(y) {
+      /* Empty */
+   }
+
+   double getX() const {
+      return x;
+   }
+
+   double getY() const {
+      return y;
+   }
+
+private:
+   double x;
+   double y;
+};
+
+class GWindow {
+public:
+   GWindow() : width(500), height(300), out("Snowflake.svg") {
+      out << "<svg xmlns=\"http://www.w3.org/2000/svg\" "
+          << "width=\"" << width << "\" "
+          << "height=\"" << height << "\" "
+          << "viewBox=\"0 0 " << width << " " << height << "\">\n";
+      out << "<rect width=\"100%\" height=\"100%\" fill=\"white\" />\n";
+   }
+
+   ~GWindow() {
+      out << "</svg>\n";
+   }
+
+   double getWidth() const {
+      return width;
+   }
+
+   double getHeight() const {
+      return height;
+   }
+
+   GPoint drawPolarLine(GPoint pt, double r, double theta) {
+      double radians = theta * 3.14159265358979323846 / 180.0;
+      GPoint end(pt.getX() + r * cos(radians),
+                 pt.getY() - r * sin(radians));
+      out << "<line x1=\"" << pt.getX()
+          << "\" y1=\"" << pt.getY()
+          << "\" x2=\"" << end.getX()
+          << "\" y2=\"" << end.getY()
+          << "\" stroke=\"black\" stroke-width=\"1.5\" />\n";
+      return end;
+   }
+
+private:
+   double width;
+   double height;
+   ofstream out;
+};
 
 /* Function prototypes */
 
